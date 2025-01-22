b5f253d86b33edb88afc19fe5a4e21bf6b348f57
Ritik Singh Kushwaha <115486545+DrSkyRitik-15@users.noreply.github.com>
2025-01-22 09:20:23 +0530

    String reverse check

---
+++ StringPallidrom.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        StringBuilder aa=new StringBuilder(A).reverse();
+        String g=aa.toString();
+        if(A .equals(g))
+        {
+            System.out.println("Yes");
+        }
+        else
+        {
+            System.out.println("No");
+        }
+        
+        /* Enter your code here. Print output to STDOUT. */
+        
+    }
+}
+
+
+
